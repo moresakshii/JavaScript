1c0c2e279ceb59c19706d10ce3ef0816ee899e44
Sakshi  More <sakshiimore87@gmail.com>
2026-07-24 08:28:53 +0000

    JavaScript Math Object

---
+++ JavaScript Math Object/Math_Object.js
@@ -0,0 +1,42 @@
+//math.PI
+console.log(Math.PI);
+
+//Math.round()
+console.log(Math.round(4.6));
+
+//math.floor()
+console.log(Math.floor(4.8));
+
+//math.ceil()
+console.log(Math.ceil(4.2));
+
+//math.trunc()
+console.log(Math.trunc(5.8));
+
+//math.abs()
+console.log(Math.abs(-20));
+
+//math.max()
+console.log(Math.max(5,8,3,7));
+
+//math.min()
+console.log(Math.min(5,8,3,7));
+
+//math.pow()
+console.log(Math.pow(2,5));
+
+//Exponent Operator(**)
+console.log(2**5);
+
+//math.sqrt()
+console.log(Math.sqrt(64));
+
+//math.cbrt()
+console.log(Math.cbrt(27));
+
+//Math.random()
+console.log(Math.random());
+
+//Random Integer(1-10)
+let random=Math.floor(Math.random()*10)+1;
+console.log(random);
